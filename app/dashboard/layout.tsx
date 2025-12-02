05f6ca745a100db8ef74f0a358a5ccdc1f9642f8
dev.sandakelum <dev.sandakelum@example.com>
2025-12-02 18:25:55 +0530

    url added to icons in sidebar

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import "./../globals.css";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      {children}
+    </SidebarProvider>
+  );
+}
